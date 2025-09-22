c5d33a52c89719aef49e3444b43299e63e95d186
dzarembo <dima.zarembo@gmail.com>
2025-09-22 17:45:40 +0300

    Refactor KafkaProducerConfig to use application.properties

--- src/main/java/com/example/kafka/KafkaProducerConfig.java
+++ src/main/java/com/example/kafka/KafkaProducerConfig.java
@@ -3,8 +3,10 @@ package com.example.kafka;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -15,14 +17,21 @@ import java.util.Map;
 
 @Configuration
 public class KafkaProducerConfig {
+    @Autowired
+    Environment environment;
+
     @Bean
     public ProducerFactory<String, Order> producerFactory(ObjectMapper objectMapper) {
         Map<String, Object> configProperties = new HashMap<>();
-        configProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "178.172.132.41:9092");
-        JsonSerializer<Order> serializer = new JsonSerializer<>(objectMapper);
-        serializer.setAddTypeInfo(false);
-
-        return new DefaultKafkaProducerFactory<>(configProperties, new StringSerializer(), serializer);
+        configProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                environment.getProperty("spring.kafka.producer.bootstrap-servers"));
+        configProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                environment.getProperty("spring.kafka.producer.key-serializer"));
+        configProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                environment.getProperty("spring.kafka.producer.value-serializer"));
+        configProperties.put(ProducerConfig.ACKS_CONFIG,
+                environment.getProperty("spring.kafka.producer.acks"));
+        return new DefaultKafkaProducerFactory<>(configProperties);
     }
 
     @Bean
